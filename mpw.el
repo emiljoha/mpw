9ad62f046c18ae659529ed73a82177a80e38f24d
Emil Johansson <emil.sweden@gmail.com>
2019-09-14 12:10:24 +0200

    Emacs package (#8)
    * Add emacs interface.
    
    * Add documentation for emacs package.

---
+++ mpw.el
@@ -0,0 +1,19 @@
+(defun async-shell-command-no-window
+    (command)
+  (interactive)
+  (let
+      ((display-buffer-alist
+        (list
+         (cons
+          "\\*Async Shell Command\\*.*"
+          (cons #'display-buffer-no-window nil)))))
+    (async-shell-command
+     command)))
+
+(defun mpw (site)
+  "MasterPassword algorithm now in emacs.
+Note that mpw need to be installed and full name configured for this function to work."
+  (interactive "sSite: ")
+  (async-shell-command-no-window (concat "mpw --quiet " site))
+  )
+
