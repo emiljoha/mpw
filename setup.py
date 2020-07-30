c09b7b3cb5b6d9833a53ae85148640b3dddcdb7b
Emil Johansson <emil.sweden@gmail.com>
2020-07-30 19:12:48 +0200

    Move algorithm to seperate repository.
    Needed to build a GUI app in a seperate repository.
    
    Also makes it more usefull to others.

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(name='mpw',
             'mpw = mpwcli:main',
             'mpw_snap = mpwcli:main_snap',
         ],},
-      py_modules=['mpwalg', 'mpwcli'],
+      py_modules=['mpwcli'],
       setup_requires=["pytest-runner"],
-      install_requires=['pyperclip', 'scrypt'],
+      install_requires=['pyperclip', 'git+https://github.com/emiljoha/pympw.git@acc48fe'],
       tests_require=['pytest', 'pexpect']
 )
